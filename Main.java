b0b24c6b545e092f6c8401ea013153f652f97403
Muhammad Imran <m.imran.ali.sajid@gmail.com>
2024-11-08 14:42:18 +0500

    initial setup

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 36;
+        String name = "M Imran";
+
+        System.out.println(name +" "+age);
+    }
+}
